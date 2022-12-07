4c637553b3427f268b26dbb952a6bf26f50a90b1
Martin <mdsugarev@gmail.com>
2022-12-07 21:01:44 +0200

    added style

---
+++ src/components/LoginForm/LoginForm.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import styles from '../components/LoginForm.module.scss'
+
+
+export default function LoginForm(){
+
+    return(
+         <form className={styles.form}>
+               <label className={styles.label} for="name" >Name</label>
+               <input className={styles.input} name="username" id="username" type="text"/>
+               <label className={styles.label} for="password">Password</label>
+               <input className={styles.input} name="password" id="password" type="password"/>
+               <button className={styles.submit} type="submit" >Submit</button>
+           </form>
+    )
+}
