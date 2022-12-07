a2ce4c637ab5684c3a0b63441bf7312b26fbabf5
Martin <mdsugarev@gmail.com>
2022-12-07 21:33:21 +0200

    aaaaa

--- src/components/LoginForm/LoginForm.js
+++ src/components/LoginForm/LoginForm.js
@@ -6,9 +6,9 @@ export default function LoginForm(){
 
     return(
          <form className={styles.form}>
-               <label className={styles.label} for="name" >Name</label>
+               <label className={styles.label}  >Name</label>
                <input className={styles.input} name="username" id="username" type="text"/>
-               <label className={styles.label} for="password">Password</label>
+               <label className={styles.label} >Password</label>
                <input className={styles.input} name="password" id="password" type="password"/>
                <button className={styles.submit} type="submit" >Submit</button>
            </form>
